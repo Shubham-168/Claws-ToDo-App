cb7bdb3e87fd783e515a6692cba5ad0c5d6c4bde
shubham <shubham@gmail.com>
2024-08-03 18:23:54 +0530

    Todo app solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import ToDo from './components/ToDo'
+import { addToDo, getAllToDo, updateToDo,deleteToDo } from './utils/HandleApi';
+
 
 function App() {
+
+  const [todo, setToDo] = useState([])
+  const [text, setText] = useState("")
+  const [isupdating, setIsUpdating] = useState(false)
+  const [toDoId, setToDoId] = useState("")
+
+
+  useEffect(() => {
+    getAllToDo(setToDo)
+  }, [])
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true)
+    setText(text)
+    setToDoId(_id)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+          <h1>ToDo App</h1>
+
+          <div className="top">
+            <input 
+            type="text" 
+            placeholder="Add ToDos..." 
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            />
+
+            <div className="add" onClick={ 
+              isupdating ?  
+              () => updateToDo(toDoId, text, setToDo, setText, setIsUpdating) 
+              : 
+              () => addToDo(text, setText, setToDo)}>
+              {isupdating ? "Update" : "Add"}
+            </div>
+
+          </div>
+
+          <div className="list">
+            {todo.map((item) => 
+            <ToDo 
+            key={item.id} 
+            text={item.text} 
+            updateMode={() => updateMode(item._id, item.text)}
+            deleteToDo={() => deleteToDo(item._id, setToDo)}
+            />)}
+
+          </div>
+
+
+      </div>
     </div>
   );
 }
